8f7194b30f83fc46e0369c80256bbf23877c262c
tsdmCode <116952397+tsdmCode@users.noreply.github.com>
2025-06-02 14:43:06 +0200

    TODO: Fix the rest of the fonts and grids

---
+++ assets/js/index.js
@@ -0,0 +1,71 @@
+const backgrounds = [
+  '/assets/_FILES/images/slider1.jpg',
+  '/assets/_FILES/images/slider2.jpg',
+  '/assets/_FILES/images/slider3.jpg',
+];
+const form = document.getElementById('subscribeform');
+const emailRegex = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+const noSpecialSignsRegex = new RegExp(/[!@$%^&*(),?":{}|<>]/);
+const header = document.getElementById('global-header');
+
+//preloader mine billeder så jeg undgår hvide flashes
+backgrounds.forEach((src) => {
+  const img = new Image();
+  img.src = src;
+});
+
+let currentIndex = 0;
+
+setInterval(() => {
+  if (currentIndex == backgrounds.length - 1) {
+    currentIndex = 0;
+
+    header.style.backgroundImage = `url(${backgrounds[currentIndex]})`;
+  } else {
+    currentIndex++;
+    header.style.backgroundImage = `url(${backgrounds[currentIndex]})`;
+  }
+}, 5000);
+
+let hasErrors = false;
+
+function submitForm(event) {
+  event.preventDefault();
+  clearErrors();
+
+  const fullName = event.target.fullname;
+  const email = event.target.email;
+
+  if (fullName.value.length < 2) {
+    setErrorMessage(fullName, 'Fornavn skal være på mere end et bogstav');
+  } else if (noSpecialSignsRegex.test(fullName.value)) {
+    setErrorMessage(fullName, 'Dit navn er for mærkeligt, DUDE!');
+  }
+
+  if (!emailRegex.test(email.value)) {
+    setErrorMessage(email, 'Indtast en gyldig e-mail!');
+  }
+
+  if (!hasErrors) {
+    alert('Tillykke du har indsendt formen');
+  }
+}
+
+function setErrorMessage(target, message) {
+  hasErrors = true;
+  let errorMessage = document.createElement('b');
+  errorMessage.classList.add('error');
+  errorMessage.innerText = message;
+  target.after(errorMessage);
+}
+
+function clearErrors() {
+  hasErrors = false;
+  let errors = document.querySelectorAll('b');
+  errors.forEach((el) => {
+    el.remove();
+  });
+}
+
+// submitBtn.addEventListener('click', submitForm);
+form.addEventListener('submit', submitForm);
